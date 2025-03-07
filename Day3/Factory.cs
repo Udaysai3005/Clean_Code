f6f69cce24d38a65e2da413649e2ecfd753e71ff
Uday <udayramesh90@gmail.com>
2025-03-07 12:36:47 +0530

    Add files via upload

---
+++ Day3/Factory.cs
@@ -0,0 +1,17 @@
+﻿namespace hamaraBasket;
+
+public class Factory
+{
+    public List<Item> PrepareItemList(string name, int sellIn, int qualityIn)
+    {
+        return new List<Item>
+        {
+            new Item { Name = name, SellIn = sellIn, Quality = qualityIn },
+        };
+    }
+    public void UpdateQualityHelper(List<Item> Items)
+    {
+        HamaraBasket app = new HamaraBasket(Items);
+        app.UpdateQuality();
+    }
+}
